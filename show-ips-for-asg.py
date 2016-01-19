aa5458e7fb1112dc4bf92db81a223f291a9e022e
Duncan Ward <git@dub.to>
2016-01-18 22:46:56 -0500

    (style) Fixups

--- show-ips-for-asg.py
+++ show-ips-for-asg.py
@@ -1,8 +1,16 @@
-#!python
+#!/usr/local/bin/python
 
 import boto.ec2
-import sys, os
+import sys
+import os
 
-conn = boto.ec2.connect_to_region(os.getenv('AWS_DEFAULT_REGION', 'us-east-1'))
-reservations = conn.get_all_instances(filters={"tag:aws:autoscaling:groupName" : sys.argv[1]})
-print (' '.join([i.private_ip_address for r in reservations for i in r.instances if i.state == 'running']))
+conn = boto.ec2.connect_to_region(
+    os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+)
+reservations = conn.get_all_instances(
+    filters={"tag:aws:autoscaling:groupName": sys.argv[1]}
+)
+print (' '.join([i.private_ip_address
+                 for r in reservations
+                 for i in r.instances
+                 if i.state == 'running']))
